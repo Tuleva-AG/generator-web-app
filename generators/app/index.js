00143ada1eeb25e4445cc620c547be7d882345e1
Sebastian  Schütze <sebastian.schuetze@tuleva.de>
2018-03-02 11:30:51 +0100

    added new prompts to generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,22 +7,33 @@ module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the superior ${chalk.red('generator-tuleva-web-app')} generator!`)
+      yosay(`Welcome to the ${chalk.red('tuleva-web-app')} generator from Tuleva!`)
     );
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true,
-        store: true
+        type: 'input',
+        name: 'name',
+        message: 'Name of your project',
+        default: this.appname
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Project description',
+        default: this.description
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'What is the authors name',
+        default: ''
       }
     ];
 
-    return this.prompt(prompts).then(props => {
+    return this.prompt(prompts).then(answers => {
       // To access props later use this.props.someAnswer;
-      this.props = props;
+      this.props = answers;
     });
   }
 
