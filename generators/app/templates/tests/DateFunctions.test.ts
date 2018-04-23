0ed4884b4314ce42c7542d85600c60e7fbadd640
Ali Coskun <ali.coskun@tuleva.de>
2018-04-23 14:22:24 +0200

    Updated test method

--- generators/app/templates/tests/DateFunctions.test.ts
+++ generators/app/templates/tests/DateFunctions.test.ts
@@ -1,12 +1,13 @@
 /// <reference types="mocha" />
+import { assert } from 'chai';
 
-import {assert} from "chai";
-import HelloDate from "./../src/components/utilities/helloDate";
+import HelloDate from './../src/app/utilities/helloDate';
+
 
 describe("Date Functions", () => {
     describe("GetCurrentDateByType", () => {
-            it("Give back the current day of the month.", () => {
-                assert.equal(HelloDate.GetCurrentDateByType("date"),(new Date).getDate().toString());
-            });
+        it("Give back the current day of the month.", () => {
+            assert.equal(HelloDate.GetCurrentDateByType("date"), (new Date).getDate().toString());
+        });
     });
 });
